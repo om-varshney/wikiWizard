4a1b059990539578e8dc27ee5d3e864eca1cb133
om-varshney <63551805+om-varshney@users.noreply.github.com>
2022-12-20 23:58:20 +0530

    Starting the build of PDFVision for Microsoft Imagine Cup 2023!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We are building <code>PDFVision</code>!
         </p>
         <a
           className="App-link"
